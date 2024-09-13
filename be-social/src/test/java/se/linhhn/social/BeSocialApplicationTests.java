f93fea35f52b94d8036b389070b1f6b0fdd7e8d1
Linh <linhhnse98@gmail.com>
2024-09-13 19:32:31 +0700

    init back-end

---
+++ be-social/src/test/java/se/linhhn/social/BeSocialApplicationTests.java
@@ -0,0 +1,13 @@
+package se.linhhn.social;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeSocialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
